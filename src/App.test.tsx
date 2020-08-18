01a9f49d6cc2761aeb0a1a0333d1c86fa9a5b25b
Jack <jackmurphy756@gmail.com>
2020-08-18 13:23:34 +0100

    replace hookrouter with raviger as hookrouter is no longer maintained

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,21 +5,50 @@ import EnzymeAdapter from "enzyme-adapter-react-16";
 import { render } from '@testing-library/react';
 import App from './App';
 
-Enzyme.configure({adapter: new EnzymeAdapter()})
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+/**
+  * Creates a shallow app component
+  * @function setup
+  * @param {object} props - Component props specific to setup.
+  * @param {any} state - Initial state for setup.
+  * @returns {ShallowWrapper}
+*/
+const setup = (props = {}, state = null) => {
+  return shallow(<App {...props} />)
+}
+/**
+  * Return ShallowWrapper containing node(s) with given data-test value.
+  * @function findTestByAttr
+  * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search inside.
+  * @param {string} val - Value of data-test attribute for search.
+  * @returns {ShallowWrapper}
+*/
+const findTestByAttr = (wrapper,val) => {
+  return wrapper.find(`[data-test="${val}"]`)
+}
 
 test('renders without error', () => {
-  const wrapper = shallow(<App/>);
-  const appComponent = wrapper.find("[data-test='component-app']")
+  const wrapper = setup();
+  const appComponent = findTestByAttr(wrapper, "component-app");
   expect(appComponent.length).toBe(1);
 });
 
 test('renders header content', () => {
+  const wrapper = setup();
+  const headerComponent = findTestByAttr(wrapper, "component-header");
+  expect(headerComponent.length).toBe(1);
 });
 
 test('renders local currency dropdown', () => {
+  const wrapper = setup();
+  const dropdownComponent = findTestByAttr(wrapper, "component-local-currency-dropdown");
+  expect(dropdownComponent.length).toBe(1);
 });
 
 test('selected local currency starts as USD', () => {
+  const wrapper = setup();
+  const initialSelectedCurrency = wrapper.state("countryCodes")
 });
 
 test('clicking local currency from list displays new selected currency', () => {
@@ -30,11 +59,22 @@ test('clicking cryptocurrency from list displays new page', () => {
 });
 
 test('renders cryptocurrency list', () => {
+  const wrapper = setup();
+  const listComponent = findTestByAttr(wrapper, "component-cryptos-list");
+  expect(listComponent.length).toBe(1);
 });
 
 test('each visible cryptocurrency in list shows data', () => {
 });
 
 // single Cryptocurrency page
-test('cryptocurrency data shows correctly', () => {
-});
+// test('renders cryptocurrency list', () => {
+//   const wrapper = setup();
+//   const appComponent = findTestByAttr(wrapper, "component-crypto");
+//   expect(appComponent.length).toBe(1);
+// });
+//
+// test('cryptocurrency data displays correctly', () => {
+// });
+
+console.log(setup().debug())
--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,39 @@ import React from 'react';
 import { Crypto, Iso } from "./types";
 import { Constants } from "./constants";
 
-import { useRoutes } from 'hookrouter';
+import { Link, useRoutes, useQueryParams } from 'raviger';
+
+const countryCodes: Iso[] = [
+  { name: "USD", isSelected: true },
+  { name: "GBP", isSelected: false },
+  { name: "EUR", isSelected: false },
+  { name: "JPY", isSelected: false },
+  { name: "KRW", isSelected: false }
+];
+
+const cryptos: Crypto[] = [{
+  Status: Constants.NOT_LOADED,
+  Name: "BCH",
+  FullName: "Bitcoin Cash",
+  MarketCap: 1000000,
+  CirculatingSupply: 2000,
+  Price: 30,
+  Volume24Hour: 3289.12309,
+  ChangePCT24Hour: 8.89
+}, {
+  Status: Constants.NOT_LOADED,
+  Name: "BTC",
+  FullName: "Bitcoin",
+  MarketCap: 1000000,
+  CirculatingSupply: 2000,
+  Price: 30,
+  Volume24Hour: 3289.12309,
+  ChangePCT24Hour: 8.89
+}];
 
 const NotFoundPage = () => (
   <p>Oops! This page no longer exists.
-     Alternatively find up to the minute cryptocurrency values <a href="/" >here</a>.
+     Alternatively find up to the minute cryptocurrency values <Link href="/">here</Link>.
   </p>)
 
 interface CurrencyProps {
@@ -15,43 +43,29 @@ interface CurrencyProps {
 }
 
 // cryptocurrencies listed on allCurrenciesPage
-const CurrencyItem = ({ currency }: CurrencyProps) => (
-  <div>
-    <p>{currency.FullName}</p>
-    <p>{currency.Price}</p>
-    <p>{currency.MarketCap}</p>
-    <p>{currency.ChangePCT24Hour}</p>
-  </div>
-)
+const CurrencyItem = ({ currency }: CurrencyProps) => {
+  let [params, setParams] = useQueryParams();
+
+  return (
+    <Link
+      href={`/single/${currency.Name.toLowerCase()}`}>
+      <p>{currency.FullName}</p>
+      <p>{currency.Price}</p>
+      <p>{currency.MarketCap}</p>
+      <p>{currency.ChangePCT24Hour}</p>
+    </Link>
+  )
+}
 
 const AllCurrenciesPage = () => {
-  const cryptos: Crypto[] = [{
-    Status: Constants.NOT_LOADED,
-    Name: "BTC",
-    FullName: "Bitcoin",
-    MarketCap: 1000000,
-    CirculatingSupply: 2000,
-    Price: 30,
-    Volume24Hour: 3289.12309,
-    ChangePCT24Hour: 8.89
-  }, {
-    Status: Constants.NOT_LOADED,
-    Name: "BTC",
-    FullName: "Bitcoin",
-    MarketCap: 1000000,
-    CirculatingSupply: 2000,
-    Price: 30,
-    Volume24Hour: 3289.12309,
-    ChangePCT24Hour: 8.89
-  }];
-  return (<>
+  return (<div data-test="component-cryptos-list">
     {cryptos.map((currency: Crypto, index: any) => (
       <div key={currency.Name}>
-        <span>{index}</span>
+        <span>{index + 1}</span>
         <CurrencyItem currency={currency} />
       </div>
     ))}
-  </>)
+  </div>)
 };
 
 
@@ -69,15 +83,14 @@ const CurrencyDetail = ({ label, detail }: DetailProps) => (
 
 const SingleCurrencyPage = ({ currency }: CurrencyProps) => {
   return (
-    <div>
+    <div data-test="component-crypto">
       <div>
         <CurrencyDetail label={"Rank"} detail={"1"} />
-
       </div>
       <div>
-        <CurrencyDetail label={"Market Cap"} detail={"123456"} />
-        <CurrencyDetail label={"24 Hour Volume %"} detail={"12%"} />
-        <CurrencyDetail label={"Circulating Supply"} detail={"456"} />
+        <CurrencyDetail label={"Market Cap"} detail={`$${currency.MarketCap}`} />
+        <CurrencyDetail label={"24 Hour Volume %"} detail={`${currency.ChangePCT24Hour}%`} />
+        <CurrencyDetail label={"Circulating Supply"} detail={`${currency.CirculatingSupply}`} />
       </div>
 
     </div>
@@ -94,38 +107,24 @@ const CurrencyTitle = ({ currency }: CurrencyProps) => (
 
 const routes = {
   '/': () => <AllCurrenciesPage />,
-  '/single/:currency': (params: any) => <SingleCurrencyPage currency={params.currency} />
+  '/single/:currency': (params: any) => {
+
+    const crypto = cryptos.filter(c => c.Name.toLowerCase() === params.currency)[0];
+    console.log(34, crypto);
+    return typeof crypto !== "undefined" ? <SingleCurrencyPage currency={crypto} /> : <NotFoundPage />
+    // return <SingleCurrencyPage />
+  }
 };
 
 function App() {
   const routeResult = useRoutes(routes);
-
-  const currency = {
-    Status: Constants.NOT_LOADED,
-    Name: "BTC",
-    FullName: "Bitcoin",
-    MarketCap: 1000000,
-    CirculatingSupply: 2000,
-    Price: 30,
-    Volume24Hour: 3289.12309,
-    ChangePCT24Hour: 8.89
-  };
-
-  const countryCodes: Iso[] = [
-    { name: "USD", isSelected: true },
-    { name: "GBP", isSelected: false },
-    { name: "EUR", isSelected: false },
-    { name: "JPY", isSelected: false },
-    { name: "KRW", isSelected: false }
-  ];
-
   return (
     <div className="App" data-test="component-app">
 
-      <div className="header">
-        {true ? <h1>VFCRYPTO</h1> : <CurrencyTitle currency={currency} />}
+      <div className="header" data-test="component-header">
+        {true ? <Link href="/"><h1>VFCRYPTO</h1></Link> : <CurrencyTitle currency={cryptos[0]} />}
 
-        <div>{countryCodes.map((iso: Iso, index: any) =>
+        <div data-test="component-local-currency-dropdown">{countryCodes.map((iso: Iso, index: any) =>
           <span key={iso.name}>{iso.name}</span>)}
         </div>
       </div>
@@ -136,3 +135,4 @@ function App() {
 }
 
 export default App;
+// onClick={(e) => console.log(e)}
